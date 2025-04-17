7c209a3d25b6fb28a6edd7fbd37224cbe45b9c35
7-second <binigossa1@gmail.com>
2025-04-17 12:01:38 +0300

    eroor d

--- src/Component/GenreMovieList.jsx
+++ src/Component/GenreMovieList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import GlobalApi from '../services/GlobalApi'; // Adjust path if needed
+ // Adjust path if needed
 
 function GenreMovieList() {
     const { genreId: routeGenreId, page: routePage } = useParams();
